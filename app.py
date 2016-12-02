1cf4d0dd05f8ff18409a637665bb9300b179b67a
Tsuru <Tsuruoka@tsuru.local>
2016-12-02 15:03:36 +0900

    separate js / css.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
 
 @app.route('/hello/<name>')
 def hello(name):
