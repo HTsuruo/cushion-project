7ff22122b4c3ffefbc2f5aec39f71876c12018f5
Tsuru <Tsuruoka@tsuru.local>
2016-12-02 15:19:36 +0900

    edit directory tree.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("content/index.html")
 
 @app.route('/hello/<name>')
 def hello(name):
@@ -18,4 +18,4 @@ def hello(name):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(port=port, debug=True)
